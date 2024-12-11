80717d4b1cbf79af7b3f4325362dc923dc71c76d
Marleen Dommerholt <m.b.dommerholt@gmail.com>
2024-12-11 10:48:29 +0100

    Create quarto document with results

--- _targets.R
+++ _targets.R
@@ -5,7 +5,8 @@
 
 # Load packages required to define the pipeline:
 library(targets)
-# library(tarchetypes) # Load other packages as needed.
+library(tarchetypes) # Load other packages as needed.
+library(quarto)
 
 # Set target options:
 tar_option_set(
@@ -67,6 +68,10 @@ list(
   tar_target(
     name = fig_metabolite_distribution,
   command = plot_distributions(lipidomics)
+  ),
+tar_quarto(
+    name = quarto_doc,
+    path = "doc/learning.qmd"
 )
 )
 
